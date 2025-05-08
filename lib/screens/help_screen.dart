aee1e70a42373cd2feac1bb347fdf0b66a0f0af4
abbymulry <amulry1@lsu.edu>
2025-05-08 12:19:30 -0500

    make buttons similar to play screen

--- lib/screens/help_screen.dart
+++ lib/screens/help_screen.dart
@@ -32,73 +32,88 @@ class HelpPage extends StatelessWidget {
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                const SizedBox(height: 40),
-                OutlinedButton(
-                  style: OutlinedButton.styleFrom(
+                SizedBox(
+                  width: double.infinity, 
+                  height: 60,             
+                  child: OutlinedButton(
+                    style: OutlinedButton.styleFrom(
                     foregroundColor: Colors.red,
-                    side: BorderSide(color: Colors.red),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(8),
-                    ),
-                  ),
-                  onPressed:
-                      () => _showHelpDialog(
-                        context,
-                        'Objective',
-                        'Be the first player to complete all ten phases by discarding your entire hand according to the specific requirements of the current phase. The player to complete all 10 phases first wins!',
-                      ),
-                  child: const Text('Objective'),
-                ),
-                const SizedBox(height: 20),
-                OutlinedButton(
-                  style: OutlinedButton.styleFrom(
-                    foregroundColor: Colors.red,
-                    side: BorderSide(color: Colors.red),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(8),
-                    ),
-                  ),
-                  onPressed:
-                      () => _showHelpDialog(
-                        context,
-                        'Setup',
-                        'Shuffle the deck and deal 10 cards face down to each player. The remaining deck is placed face down in the center, forming the draw pile. Flip the top card of the draw pile over to reveal the discard pile.',
-                      ),
-                  child: const Text('Setup'),
-                ),
-                const SizedBox(height: 20),
-                OutlinedButton(
-                  style: OutlinedButton.styleFrom(
-                    foregroundColor: Colors.red,
-                    side: BorderSide(color: Colors.red),
+                    side: const BorderSide(color: Colors.red),
+                    textStyle: const TextStyle(fontSize: 18),
                     shape: RoundedRectangleBorder(
                       borderRadius: BorderRadius.circular(12),
                     ),
+                  ),  
+                   onPressed: () => _showHelpDialog(
+                    context,
+                    'Objective',
+                    'Be the first player to complete all ten phases by discarding your entire hand...',
                   ),
-                  onPressed:
-                      () => _showHelpDialog(
-                        context,
-                        'Rules',
-                        'At the beginning of your turn you will draw a card from the draw pile. At the end of your turn you will discard a card into the discard pile. During your turn you will attempt to discard as many cards as you can while meeting the requirements of the current phase. Requirements can include a run (a sequence of cards of the same suit ascending or descending), or a set (3 or 4 of the same number). Once you discard all your cards for the current phase, you will move onto the next phase. The player to complete all 10 phases first wins!',
-                      ),
-                  child: const Text('Rules'),
+                    child: const Text('Objective'),
+                  ),
                 ),
-                const SizedBox(height: 20),
-                OutlinedButton(
-                  style: OutlinedButton.styleFrom(
+
+                SizedBox(
+                  width: double.infinity, 
+                  height: 60,             
+                  child: OutlinedButton(
+                    style: OutlinedButton.styleFrom(
                     foregroundColor: Colors.red,
-                    side: BorderSide(color: Colors.red),
+                    side: const BorderSide(color: Colors.red),
+                    textStyle: const TextStyle(fontSize: 18),
                     shape: RoundedRectangleBorder(
                       borderRadius: BorderRadius.circular(12),
                     ),
+                  ),  
+                   onPressed: () => _showHelpDialog(
+                    context,
+                    'Setup',
+                    'Shuffle the deck and deal 10 cards face down to each player. The remaining deck is placed face down in the center, forming the draw pile. Flip the top card of the draw pile over to reveal the discard pile.',
                   ),
-                  onPressed:
-                      () => _showHelpDialog(
-                        context,
-                        'Special Cards',
-                        'Wild Card: Can be played as any number or color to complete a run or set.\n\nSkip Card: Place this card on top of the discard pile to skip the next player\'s turn. The next player draws 2 cards and completes their turn.',
-                      ),
-                  child: const Text('Special Cards'),
+                    child: const Text('Setup'),
+                  ),
+                ),
+
+                SizedBox(
+                  width: double.infinity, 
+                  height: 60,             
+                  child: OutlinedButton(
+                    style: OutlinedButton.styleFrom(
+                    foregroundColor: Colors.red,
+                    side: const BorderSide(color: Colors.red),
+                    textStyle: const TextStyle(fontSize: 18),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                  ),  
+                   onPressed: () => _showHelpDialog(
+                    context,
+                    'Rules',
+                    'At the beginning of your turn you will draw a card from the draw pile. At the end of your turn you will discard a card into the discard pile. During your turn you will attempt to discard as many cards as you can while meeting the requirements of the current phase. Requirements can include a run (a sequence of cards of the same suit ascending or descending), or a set (3 or 4 of the same number). Once you discard all your cards for the current phase, you will move onto the next phase. The player to complete all 10 phases first wins!',
+                  ),
+                    child: const Text('Rules'),
+                  ),
+                ),
+
+                SizedBox(
+                  width: double.infinity, 
+                  height: 60,             
+                  child: OutlinedButton(
+                    style: OutlinedButton.styleFrom(
+                    foregroundColor: Colors.red,
+                    side: const BorderSide(color: Colors.red),
+                    textStyle: const TextStyle(fontSize: 18),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                  ),  
+                   onPressed: () => _showHelpDialog(
+                    context,
+                    'Special Cards',
+                    'Wild Card: Can be played as any number or color to complete a run or set.\n\nSkip Card: Place this card on top of the discard pile to skip the next player\'s turn. The next player draws 2 cards and completes their turn.',
+                  ),
+                    child: const Text('Special Cards'),
+                  ),
                 ),
               ],
             ),
