742fdcf13251ea4bebc51170c2f27fb50263b8e0
christianherpin <108888501+christianherpin@users.noreply.github.com>
2025-05-08 18:37:14 -0500

    add end of game jingle

--- lib/services/sound_player.dart
+++ lib/services/sound_player.dart
@@ -3,12 +3,21 @@ import 'package:audioplayers/audioplayers.dart';
 class SoundPlayer {
   static final AudioPlayer _player = AudioPlayer();
 
-  static Future<void> playSuccessSound() async {
+  static Future<void> playPhaseCompleteSound() async {
     try {
       await _player.stop(); // Stop any current sound
-      await _player.play(AssetSource('assets/sounds/success.ogg'));
+      await _player.play(AssetSource('assets/sounds/levelsuccess.wav'));
     } catch (e) {
       print('Error playing success sound: $e');
+    }
+  }
+
+  static Future<void> playGameCompleteSound() async {
+    try {
+      await _player.stop();
+      await _player.play(AssetSource('sounds/success.ogg'));
+    } catch (e) {
+      print('Error playing game complete sound: $e');
     }
   }
 }
